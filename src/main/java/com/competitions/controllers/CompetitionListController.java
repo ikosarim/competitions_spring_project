49f7ef681366ff6004ae26dec049a8e616d55e3a
Igor <igor.kosarim@yandex.ru>
2019-11-11 00:08:12 +0300

    competition_list in work

--- src/main/java/com/competitions/controllers/CompetitionListController.java
+++ src/main/java/com/competitions/controllers/CompetitionListController.java
@@ -1,6 +1,7 @@
 package com.competitions.controllers;
 
 import com.competitions.entities.Captain;
+import com.competitions.entities.Competition;
 import com.competitions.facade.CompetitionsFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("/competitions_list")
@@ -21,15 +24,22 @@ public class CompetitionListController {
 
     @GetMapping
     public String getCompetitionsList(Model uiModel) {
-        uiModel.addAttribute("competitions", competitionsFacade.findAllCompetitions());
-        return "/competitions_list";
+//        uiModel.addAttribute("competitions", competitionsFacade.findAllCompetitions());
+        ArrayList<Competition> comp = new ArrayList<>(){{
+            add(Competition.builder().competitionName("1").competitionDescription("adj").competitionReward("jahsdg").build());
+            add(Competition.builder().competitionName("2").competitionDescription("asdfD").competitionReward("asdfwea").build());
+            add(Competition.builder().competitionName("3").competitionDescription("cwadasd").competitionReward("asdfsad").build());
+        }};
+//        uiModel.addAttribute("competitions", competitionsFacade.findAllCompetitions());
+        uiModel.addAttribute("competitions", comp);
+        return "/pages/competitions_list";
     }
 
-//    @GetMapping
-//    @GetMapping(value = "/{idCompetition}")
-    public String getCompetitionInfo(Model uiModel, @PathVariable Integer idCompetition) {
-        uiModel.addAttribute("competition", competitionsFacade.getCompetitionInfo(idCompetition));
-        return "/competition_info";
+    @GetMapping(params = {"infoOfCompetition"})
+    public String getCompetitionInfo(Model uiModel, @ModelAttribute String competitionName) {
+//        uiModel.addAttribute("competition", competitionsFacade.getCompetitionInfo(idCompetition));
+        System.out.println();
+        return "/pages/competition_info";
     }
 
 //    @GetMapping

736f532e721c4944256678e784116affabf2e263
Rocky-po <igor.kosarim@yandex.ru>
2019-12-05 15:33:14 +0300

    * use SpringBootServletInitializer

--- src/main/java/com/competitions/CompetitionsApplication.java
+++ src/main/java/com/competitions/CompetitionsApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class CompetitionsApplication extends SpringBootServletInitializer {
@@ -15,5 +18,15 @@ public class CompetitionsApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CompetitionsApplication.class, args);
+    }
+
+    @Controller
+    @RequestMapping("/")
+    public class StartController {
+
+        @GetMapping
+        public String start() {
+            return "/pages/index";
+        }
     }
 }
